59e937b8160b08e423bbb86890ab5432ef714dc0
Gregs <gregory.gerard@wister.fr>
2017-09-27 00:30:33 +0200

    Vagrant update boot ok

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -2,6 +2,7 @@
 ROOT_PASSWD='wako'
 MASTER_IP='192.168.1.220'
 
+
 # Definition des mot de passe utilisateurs - root
 echo root:$ROOT_PASSWD | chpasswd
 apt-get install -y sudo
@@ -13,7 +14,12 @@ if [ -f /vagrant/ssh/id_rsa.pub ]; then
     cp /vagrant/ssh/id_rsa.pub  /root/.ssh/authorized_keys
 fi
 
-apt-get install -y git
+
+wget -O - https://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
+echo 'deb http://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest xenial main' > /etc/apt/sources.list.d/saltstack.list
+apt-get update
+
+apt-get install -y git htop
 
 git clone https://github.com/wako057/nuxminimal.git && nuxminimal/init.sh
 
